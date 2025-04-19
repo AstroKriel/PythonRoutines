0a7b296b1b4609219139ed6f288777a0ba3d80e0
Neco Kriel <necokriel@gmail.com>
2025-04-19 13:26:51 +1000

    added more symlinks that need to be created

--- dev/setup_symlinks.sh
+++ dev/setup_symlinks.sh
@@ -2,18 +2,20 @@
 
 cd "$(dirname "$0")/.." # move to `sindri`
 
-# Link `PowerSpectra` to `loki`
-if [ ! -L PowerSpectra ]; then
-  ln -s loki PowerSpectra
-  echo "Created symlink: PowerSpectra -> loki"
-else
-  echo "Symlink already exists: PowerSpectra"
-fi
+# array of "symlink_name:repo_name" pairs
+declare -a LINKS=(
+  "PowerSpectra:loki"
+  "CorrelationFunctions:thor"
+  "EnergyTransfer:valkyrja"
+)
 
-# Link `CorrelationFunctions` to `thor`
-if [ ! -L CorrelationFunctions ]; then
-  ln -s thor CorrelationFunctions
-  echo "Created symlink: CorrelationFunctions -> thor"
-else
-  echo "Symlink already exists: CorrelationFunctions"
-fi
+for pair in "${LINKS[@]}"; do
+  IFS=":" read -r symlink target <<< "$pair"
+
+  if [ ! -L "$symlink" ]; then
+    ln -s "$target" "$symlink"
+    echo "Created symlink: $symlink -> $target"
+  else
+    echo "Symlink already exists: $symlink"
+  fi
+done
