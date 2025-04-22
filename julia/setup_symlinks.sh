6d1924240e2e0cd2f4d9ce6165a7ab9b3203678c
Neco Kriel <necokriel@gmail.com>
2025-04-22 22:06:08 +1000

    add script to setup meaningful symlinks

---
+++ julia/setup_symlinks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# array of "symlink_name:repo_name" pairs
+declare -a LINKS=(
+  "PowerSpectra:loki"
+  "CorrelationFunctions:thor"
+  "EnergyTransfer:valkyrja"
+)
+
+for pair in "${LINKS[@]}"; do
+  IFS=":" read -r symlink target <<< "$pair"
+
+  if [ ! -L "$symlink" ]; then
+    ln -s "$target" "$symlink"
+    echo "Created symlink: $symlink -> $target"
+  else
+    echo "Symlink already exists: $symlink"
+  fi
+done
